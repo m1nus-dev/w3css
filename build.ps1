4339698fb060466bf24c7332346b80020944f342
m1nus <m1nus49961@gmail.com>
2022-12-07 10:26:56 -0600

    initial commit

---
+++ build.ps1
@@ -0,0 +1,3 @@
+node-sass src\sass\sass\w3.scss src\pages\css\w3.css
+css-minify --file src\pages\css\w3.css -o src\pages\css\
+copy .\src\pages\css\*.css .\dist\
---
+++ src/sass/README.md
@@ -0,0 +1,8 @@
+# w3scss
+W3.SCSS - SASS version of W3.CSS - in development
+
+Inspired by the Official framework added by author Jan Egil Refsnes.
+
+
+
+
